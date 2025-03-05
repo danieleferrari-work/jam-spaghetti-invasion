d2a725507efce517a093ef5350d7120076538d23
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2025-03-05 11:52:06 +0100

    fix always look at camera script

--- Assets/_Project/Scripts/Utilities/AlwaysLookAtCamera.cs
+++ Assets/_Project/Scripts/Utilities/AlwaysLookAtCamera.cs
@@ -2,28 +2,23 @@ using UnityEngine;
 
 public class AlwaysLookAtCamera : MonoBehaviour
 {
-
-    [SerializeField] bool rotateXAxis;
-    [SerializeField] bool rotateYAxis;
-    [SerializeField] bool rotateZAxis;
-
-
     Camera mainCamera;
-    Quaternion startRotation;
 
     void Awake()
     {
         mainCamera = Camera.main;
-        startRotation = transform.rotation;
 
         if (mainCamera == null)
+        {
+            Debug.LogWarning("Missing Main Camera in scene");
             this.enabled = false;
+        }
     }
 
     void FixedUpdate()
     {
         Vector3 dir = mainCamera.transform.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(dir, Vector3.up) * startRotation;
+        Quaternion rotation = Quaternion.LookRotation(dir, Vector3.up);
         transform.rotation = rotation;
     }
 }
