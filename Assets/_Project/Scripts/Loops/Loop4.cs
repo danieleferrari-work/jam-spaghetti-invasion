41467a938015ddf4e27231ac4946ade8cf3c915e
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2025-03-06 11:08:16 +0100

    add empty loops

---
+++ Assets/_Project/Scripts/Loops/Loop4.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop4 : MonoBehaviour, ILoop
+{
+    public bool IsComplete()
+    {
+        return true;
+    }
+}
---
+++ Assets/_Project/Scripts/Loops/Loop7.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop7 : MonoBehaviour, ILoop
+{
+    public bool IsComplete()
+    {
+        return true;
+    }
+}
