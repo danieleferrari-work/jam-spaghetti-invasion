ae9d446f1437c3b2f1d9e4f6f5f7b8a0f87572c7
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2025-03-07 23:46:54 +0100

    update params

--- Assets/_Project/Scripts/Params.cs
+++ Assets/_Project/Scripts/Params.cs
@@ -9,21 +9,23 @@ public class Params : Singleton<Params>
     public float minFov;
     public float zoomInSpeed;
     public float zoomOutSpeed;
-
+    
+    [Space(10)]
     [Header("Gondola Movement Settings")]
-
     public float gondolaMaxSpeed;
     public float gondolaRotationSpeed;
     public float gondolaDefaultAcceleration;
+
+    [Header("Gondola Rowing Settings")]
     public float rowPushForce;
     public float rowPushDelay;
     public float rowPushDuration;
 
-    [Header("Bobbing Settings")]
+    [Header("Gondola Bobbing Settings")]
     public float gondolaBobbingFrequency;
     public float gondolaBobbingAltitude;
 
-    [Header("Rolling Settings")]
+    [Header("Gondola Rolling Settings")]
     public float gondolaRollingFrequency;
     public float gondolaRollingAmplitude;
 }
